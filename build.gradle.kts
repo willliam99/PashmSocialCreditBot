a69e1ba06a56844af7f197661c6027f3e2c496e8
William <william.gates.3299@gmail.com>
2026-09-19 15:45:03 +0330

    Upgraded kotlin to 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.4.0"
+    kotlin("jvm") version "2.4.20"
     application
     alias(libs.plugins.kotlin.serialization)
 }
